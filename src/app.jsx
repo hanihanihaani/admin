6e529bef3097a1d8c59b592babb273f2491da70b
jason zhang <2466835106@qq.com>
2018-10-17 15:23:28 +0800

    环境的搭建和webpack的配置

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+import './index.css';
+
+ReactDom.render(
+    <div>
+        <h1>hello world!</h1>,
+    </div>,
+    document.getElementById('app')
+)
